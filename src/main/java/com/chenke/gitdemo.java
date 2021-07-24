07d366df74cfa8032d7277d408b07b800fdd62f3
chenke <553490664@qq.com>
2021-07-24 12:43:22 +0800

    three commit

--- src/main/java/com/chenke/gitdemo.java
+++ src/main/java/com/chenke/gitdemo.java
@@ -3,5 +3,6 @@ package com.chenke;
 public class gitdemo {
     public static void main(String[] args) {
         System.out.println("first test");
+        System.out.println("two test");
     }
 }
